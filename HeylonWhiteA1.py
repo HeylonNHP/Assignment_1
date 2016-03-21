ba4006d5f12ad5312ca03dd3806846cc6c2933b2
Heylon <heylon96@hotmail.com>
2016-03-21 17:58:06 +1000

    Added github url

--- HeylonWhiteA1.py
+++ HeylonWhiteA1.py
@@ -2,11 +2,16 @@
 CP1404 Assignment 1 - 2016
 Items for hire
 Heylon White
+https://github.com/HeylonNHP/Assignment_1
 21/03/16
 
 
 Pseudocode:
 
+function main()
+    display welcome message
 
+    load items from csv file
+    display amount of items loaded
 """
 
