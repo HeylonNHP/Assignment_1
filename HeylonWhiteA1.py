78b58390ea80605ea1a1fff1ee14a4fcda9bed0c
Heylon <heylon.white@my.jcu.edu.au>
2016-04-04 18:31:19 +1000

    Initial planning for main() complete

--- HeylonWhiteA1.py
+++ HeylonWhiteA1.py
@@ -4,14 +4,37 @@ Items for hire
 Heylon White
 https://github.com/HeylonNHP/Assignment_1
 21/03/16
+"""
 
 
-Pseudocode:
+"""
+Pseudocode function main:
 
 function main()
     display welcome message
 
     load items from csv file
     display amount of items loaded
+
+    display menu
+    get user_input
+    while not user_input (to lowercase) == "q"
+        if user_input (to lowercase) == "l"
+            display all items
+        else if user_input (to lowercase) == "h"
+            (hire an item)
+        else if user_input (to lowercase) == "r"
+            (return an item)
+        else if user_input (to lowercase) == "a"
+            (add new item to stock)
+
+    (save to csv)
+    display amount of items saved message
+    display goodbye message
+
 """
+
+
+#rgewegre
+
 
