4e7d648e6d2c4d9ffd7d87dbe0b9cb43735e7c23
Heylon <heylon.white@my.jcu.edu.au>
2016-04-04 20:28:04 +1000

    load_items_from_csv() pseudocode complete

--- HeylonWhiteA1.py
+++ HeylonWhiteA1.py
@@ -6,6 +6,22 @@ https://github.com/HeylonNHP/Assignment_1
 21/03/16
 """
 
+"""
+Pseudocode function load_items_from_csv:
+
+function load_items_from_csv(filename)
+    file_lines_list = []
+    open filename as file_in for reading
+    for each line in filename.readlines()
+        strip white spaces and new line chars from line
+        split line by "," and set to line_item_list
+        append line_item_list to file_lines_list
+    close file_in
+    return file_lines_list
+"""
+def load_items_from_csv(filename):
+    pass
+
 
 """
 Pseudocode function main:
@@ -36,7 +52,10 @@ function main()
     display goodbye message
 
 """
+def main():
+    pass
 
+main()
 
 #rgewegre
 
