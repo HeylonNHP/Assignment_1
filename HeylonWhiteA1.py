dc68ed3e0550e32796a5809b28ad27d2be7df33b
Heylon <heylon.white@my.jcu.edu.au>
2016-04-05 19:49:13 +1000

    load_items complete list items complete

--- HeylonWhiteA1.py
+++ HeylonWhiteA1.py
@@ -7,20 +7,27 @@ https://github.com/HeylonNHP/Assignment_1
 """
 
 """
-Pseudocode function load_items_from_csv:
+Pseudocode function load_items:
 
-function load_items_from_csv(filename)
+function load_itemsfilename)
     file_lines_list = []
     open filename as file_in for reading
     for each line in filename.readlines()
         strip white spaces and new line chars from line
-        split line by "," and set to line_item_list
+        line_item_list = split line by ","
         append line_item_list to file_lines_list
     close file_in
     return file_lines_list
 """
-def load_items_from_csv(filename):
-    pass
+def load_items(filename):
+    file_lines_list = []
+    file_in = open(filename, "r")
+    for line in file_in.readlines():
+        line.strip()
+        line_item_list = line.split(",")
+        file_lines_list.append(tuple(line_item_list))
+    file_in.close()
+    return file_lines_list
 
 
 """
@@ -29,14 +36,17 @@ Pseudocode function main:
 function main()
     display welcome message
 
-    load items from csv file
-    display amount of items loaded
+    items_list = load_items(FILENAME)
+    display length of items_list + "items loaded from items.csv"
 
     display menu
     get user_input
     while not user_input (to lowercase) == "q"
         if user_input (to lowercase) == "l"
-            display all items
+            display items on file
+            count = 0
+            for each item in items_list
+                display count + " - " + item[0] + " (" + item[1] + ") = $ " + item[3]
         else if user_input (to lowercase) == "h"
             (hire an item)
         else if user_input (to lowercase) == "r"
